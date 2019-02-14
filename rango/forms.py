fea6d22bcd6771efa5a8226f658d7b30e237dea5
Adi Dharma Pramudita <2352422p@student.gla.ac.uk>
2019-02-14 19:18:15 +0000

    Edit index page

--- rango/forms.py
+++ rango/forms.py
@@ -3,13 +3,10 @@ from django.contrib.auth.models import User
 from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=
-                           Category._meta.get_field('name').max_length,
+    name = forms.CharField(max_length=128,
                            help_text="Please enter the category name.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=
-                           Category._meta.get_field('views').default)
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=
-                           Category._meta.get_field('likes').default)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     #An inline class to provide additional information to the form 
--- rango/models.py
+++ rango/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True, unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -26,7 +26,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    
+    views = models.IntegerField(default=0)
 
     def __str__(self):
     	return self.title
